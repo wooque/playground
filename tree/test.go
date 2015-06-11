fad4a8811273f208464c5cac4a3597f89e813602
Vuk Mirovic <wooque@gmail.com>
2015-06-11 17:31:19 +0200

    Replace tabs with spaces

--- tree/test.go
+++ tree/test.go
@@ -1,33 +1,33 @@
 package main;
 
 import (
-	"fmt"
+    "fmt"
 )
 
 type Node struct {
-	val int
-	left, right *Node
+    val int
+    left, right *Node
 }
 
 func expand(val int, level int) *Node {
-	if level == 0 {
-		return nil
-	}
-	return &Node{val, expand(val, level - 1), expand(val, level - 1)}
+    if level == 0 {
+        return nil
+    }
+    return &Node{val, expand(val, level - 1), expand(val, level - 1)}
 }
 
 func sumit(node *Node) int {
-	if node == nil {
-		return 0
-	}
-	return node.val + sumit(node.left) + sumit(node.right)
+    if node == nil {
+        return 0
+    }
+    return node.val + sumit(node.left) + sumit(node.right)
 }
 
 func main() {
-	for i := 1; i < 101; i++ {
-		for j := 1; j < 21; j++ {
-			result := sumit(expand(25 - j, j))
-			fmt.Printf("%d: %d\n", j, result)
-		}
-	}
+    for i := 1; i < 101; i++ {
+        for j := 1; j < 21; j++ {
+            result := sumit(expand(25 - j, j))
+            fmt.Printf("%d: %d\n", j, result)
+        }
+    }
 }
