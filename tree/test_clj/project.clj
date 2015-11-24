8f2381ea3a3468c32e74eee8276787323c8f5db7
Vuk Mirovic <wooque@gmail.com>
2015-11-24 02:00:33 +0100

    Update clojure version

--- tree/test_clj/project.clj
+++ tree/test_clj/project.clj
@@ -1,4 +1,4 @@
 (defproject test_clj "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :main test_clj.core
   :profiles {:uberjar {:aot :all}})
